9a8e1a679c77002c68b57f901400b0fece9a2e24
ktochow1 <ktochow1@binghamton.edu>
2018-07-13 18:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ function whisper(string) {
 function logShout(string) {
   console.log.toUpperCase
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())  {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
+
+  function logShout(string) {
+    if (string === string.toUpperCase())
+    console.log.toUpperCase {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logShout('hello')
+
+    expect(spy).toHaveBeenCalledWith('HELLO')
+
+    console.log.restore()
+  })
+})
