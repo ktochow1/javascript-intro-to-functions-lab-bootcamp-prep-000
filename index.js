b4c1cb7aa8d1e91e7a2ed7117421b359ac61e568
ktochow1 <ktochow1@binghamton.edu>
2018-07-13 19:15:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function sayHiToGrandma(string) {
 
   function logShout(string) {
   if (string === string.toUpperCase())
-    console.log.toUpperCase
-
+    console.log(string.toUpperCase)
+    
+    
     //const spy = expect.spyOn(console, 'log').andCallThrough()
 
     //logShout('hello')
